f42e95559cecf3c64120692c0160806be6586b8f
Jose-amlo <82465006+Jose-amlo@users.noreply.github.com>
2022-09-27 13:00:40 -0300

    Update venta.model.js

--- app/models/venta.model.js
+++ app/models/venta.model.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
-    const Venta = sequelize.define("venta", {
-      mes_venta: {
-        type: Sequelize.ENUM,
-        primaryKey: false
-      },
-      monto_venta: {
-        type: Sequelize.INT
-      }
-    });
-    return Venta;
-  };
+  const Venta = sequelize.define("venta", {
+    mes_venta: {
+      type: Sequelize.STRING,
+      primaryKey: false
+    },
+    monto_venta: {
+      type: Sequelize.INTEGER
+    }
+  });
+  return Venta;
+};
