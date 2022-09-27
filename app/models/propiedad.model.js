6dfdd2d5e3c7ce7dd44995b9ca52a607dcb226f1
JavierVaras <74783586+JavierVaras@users.noreply.github.com>
2022-09-27 00:23:55 -0300

    Create propiedad.model.js

---
+++ app/models/propiedad.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const Propiedad = sequelize.define("usuario", {
+      id_propiedad: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      region: {
+        type: Sequelize.STRING
+      },
+      comuna: {
+        type: Sequelize.STRING
+      },
+      calle: {
+        type: Sequelize.STRING
+      },
+      numero: {
+        type: Sequelize.INTEGER
+      }
+    });
+    return Propiedad;
+  };
