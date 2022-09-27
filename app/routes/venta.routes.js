cc393c1689ec34d28e0b84d05625eaaf03be34df
JavierVaras <74783586+JavierVaras@users.noreply.github.com>
2022-09-27 12:52:40 -0300

    Update venta.routes.js

--- app/routes/venta.routes.js
+++ app/routes/venta.routes.js
@@ -1,1 +1,18 @@
-
+module.exports = app => {
+    const venta = require("../controllers/venta.controller.js");
+    var router = require("express").Router();
+    // Crear un venta
+    router.post("/", venta.create);
+    // buscar venta (con condición)
+    router.get("/", venta.findAll);
+    // buscar venta con id
+    router.get("/:id_venta", venta.findOne);
+    // actualizar venta
+    router.put("/:id_venta", venta.update);
+    // eliminar venta con id
+    router.delete("/:id_venta", venta.delete);
+    // eliminar todos los venta
+    router.delete("/", venta.deleteAll);
+    // agregar rutas al servidor
+    app.use('/api/venta', router);    
+ };
