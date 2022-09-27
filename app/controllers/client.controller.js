c75d1117f7c825da92541f456bf3f771db7a3e47
KevinC001 <112519895+KevinC001@users.noreply.github.com>
2022-09-27 00:32:55 -0300

    Update client.controller.js

--- app/controllers/client.controller.js
+++ app/controllers/client.controller.js
@@ -30,40 +30,7 @@ exports.create = (req, res) => {
         });
     });
 };
-// Retornar los clientes de la base de datos.
-exports.findAll = (req, res) => {
-    const nombre_usuario = req.query.nombre_usuario;
-    var condition = nombre_usuario ? { nombre_usuario: { [Op.like]: `%${nombre_usuario}%` } } : null;
-    Usuario.findAll({ where: condition })
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Error en la búsqueda"
-        });
-    });
-};
-// Buscar un cliente por su id
-exports.findOne = (req, res) => {
-    const nombre_usuario = req.params.nombre_usuario;
-    Usuario.findByPk(nombre_usuario)
-    .then(data => {
-        if (data) {
-            res.send(data);
-        } else {
-            res.status(404).send({
-                message: `No se encontró al cliente.`
-            });
-         }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: "Error en la búsqueda"
-        });
-    });
-};
+
 // actualizar un cliente por su id
 exports.update = (req, res) => {
     const nombre_usuario = req.params.nombre_usuario;
@@ -86,43 +53,4 @@ exports.update = (req, res) => {
             message: "Error en actualización"
         });
     });
-};
-// eliminar un cliente
-exports.delete = (req, res) => {
-    const nombre_usuario = req.params.nombre_usuario;
-    Usuario.destroy({
-        where: { nombre_usuario: nombre_usuario }
-    })
-    .then(num => {
-        if (num == 1) {
-            res.send({
-                message: "Cliente eliminado"
-            });
-        } else {
-            res.send({
-                message: `Cliente no encontrado`
-            });
-        }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: "Error al eliminar cliente"
-        });
-    });
-};
-// eliminar a todos los clientes
-exports.deleteAll = (req, res) => {
-    Usuario.destroy({
-        where: {},
-        truncate: false
-    })
-    .then(nums => {
-        res.send({ message: `${nums} clientes eliminados!` });
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Error al eliminar a todos los clientes."
-        });
-    });     
 };
