7c73e5a8f70a5c38e1b92f458f887393873ef2da
Jose-amlo <82465006+Jose-amlo@users.noreply.github.com>
2022-09-27 00:26:15 -0300

    Update cobertura.model.js

--- app/models/cobertura.model.js
+++ app/models/cobertura.model.js
@@ -1,18 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
-    const Usuario = sequelize.define("usuario", {
-      nombre_usuario: {
-        type: Sequelize.STRING,
-        primaryKey: true
-      },
-      fecha_nacimiento: {
-        type: Sequelize.DATE
-      },
-      correo: {
-        type: Sequelize.STRING
-      },
-      telefono_usuario: {
-        type: Sequelize.INTEGER
-      }
+    const Cobertura = sequelize.define("cobertura", {
+        Tipo_cobertura: {
+            type: Sequelize.STRING,
+            primaryKey: true
+        }
     });
-    return Usuario;
-  };
+    return Cobertura;
+};
