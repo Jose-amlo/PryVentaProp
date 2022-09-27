7049fd9bdeac6fef17768af623c77c04c2cc8faa
Jose-amlo <82465006+Jose-amlo@users.noreply.github.com>
2022-09-27 12:59:07 -0300

    Update seguro.model.js

--- app/models/seguro.model.js
+++ app/models/seguro.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
-    const Seguro = sequelize.define("usuario", {
+    const Seguro = sequelize.define("seguro", {
       id_seguro: {
         type: Sequelize.INTEGER,
         primaryKey: true
