43f7cddc8cb5bb2f7e2a68371f42b084e195b301
JavierVaras <74783586+JavierVaras@users.noreply.github.com>
2022-09-27 13:07:09 -0300

    Update cobertura.routes.js

--- app/routes/cobertura.routes.js
+++ app/routes/cobertura.routes.js
@@ -1,1 +1,18 @@
-
+module.exports = app => {
+    const cobertura = require("../controllers/cobertura.controller.js");
+    var router = require("express").Router();
+    // Crear un cobertura
+    router.post("/", cobertura.create);
+    // buscar cobertura (con condición)
+    router.get("/", cobertura.findAll);
+    // buscar cobertura con id
+    router.get("/:id", cobertura.findOne);
+    // actualizar cobertura
+    router.put("/:id", cobertura.update);
+    // eliminar cobertura con id
+    router.delete("/:id", cobertura.delete);
+    // eliminar todos los cobertura
+    router.delete("/", cobertura.deleteAll);
+    // agregar rutas al servidor
+    app.use('/api/cobertura', router);    
+ };
