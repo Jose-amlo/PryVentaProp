3fff9d6c2c602cab9125f7638bf38b49ae2ab5db
Jose-amlo <82465006+Jose-amlo@users.noreply.github.com>
2022-09-29 15:35:34 -0300

    Update usuario.model.js

--- app/models/usuario.model.js
+++ app/models/usuario.model.js
@@ -1,18 +1,18 @@
 module.exports = (sequelize, Sequelize) => {
-    const Usuario = sequelize.define("usuario", {
-      nombre_usuario: {
-        type: Sequelize.STRING,
-        primaryKey: true
-      },
-      fecha_nacimiento: {
-        type: Sequelize.DATE
-      },
-      correo: {
-        type: Sequelize.STRING
-      },
-      telefono_usuario: {
-        type: Sequelize.INTEGER
-      }
+    const Usuario = sequelize.define("usuarios", {
+        nombre_usuario: {
+            type: Sequelize.STRING,
+            primaryKey: true
+        },
+        fecha_nacimiento: {
+            type: Sequelize.DATEONLY 
+        },
+        correo: {
+            type: Sequelize.STRING
+        },
+        telefono_usuario: {
+            type: Sequelize.INTEGER
+        }
     });
     return Usuario;
-  };
+};
